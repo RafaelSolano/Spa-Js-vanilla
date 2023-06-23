af0b5a55aeec177e72f9bc82d6e327b3304f2578
Rafael Solano <rafaelsolanol007@gmail.com>
2023-06-22 21:06:02 -0500

    Se crea el header

---
+++ src/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  const view =`
+  <div class='Header-main'>
+    <div class='header__logo'>
+      <h1>
+        <a href="/"> 100tifico</a>
+      </h1>
+    </div>
+  </div>
+  <div class='Header__nav'>
+    <a href="#/about">About</a>
+  </div>
+  `;
+  return view;
+};
+export default Header
